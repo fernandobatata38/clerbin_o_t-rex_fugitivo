df8fee2fd69539633406b0d9287c7b1ddddb3035
fernandobatata38 <101750484+fernandobatata38@users.noreply.github.com>
2022-03-22 16:58:39 -0300

    aula dia 21

--- sketch.js
+++ sketch.js
@@ -2,31 +2,38 @@ var chao ,ground2;
 var trex ,trex_correndo;
 
 function preload(){
-  trex_correndo = loadAnimation("imgs/trex1.png", "imgs/trex3.png", "imgs/trex4.png");
-  ground2=loadAnimation("imgs/ground2.png");
+  trex_correndo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  ground2=loadImage("ground2.png");
 }
 
 function setup(){
   createCanvas(600,200);
   
   //crie um sprite de trex
-  trex = createSprite(50,157,20,50);
+  trex = createSprite(50,100,20,50);
   trex.addAnimation("correndo", trex_correndo);
-  
+  trex.scale=0.6
   chao=createSprite(200,180,400,20);
-  chao.addAnimation("ground2",ground2);
+  chao.addImage("ground2",ground2);
   
 }
 
 function draw(){
   trex.collide(chao);
   pulo();
+  chao_movimentando();
   background("white");
   drawSprites();
 }
 function pulo(){
-  if(keyDown(UP_ARROW)){
+  if(keyDown("up")){
     trex.velocityY=-10;
   }
-  trex.velocityY=+7;
+  trex.velocityY+=0.8;
+}
+function chao_movimentando(){
+  chao.velocityX=-3
+  if(chao.x<0){
+    chao.x=chao.width/2
+  }
 }
