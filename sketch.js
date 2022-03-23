8d1cf88a187a8f50d1547b48ddb27b940316106a
fernandobatata38 <101750484+fernandobatata38@users.noreply.github.com>
2022-03-23 18:46:51 -0300

    continuamos o jogo e fizemos q o t rex não quebrasse 999 leis da fisica pulando.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var chao ,ground2;
 var trex ,trex_correndo;
-
+var chao_invisivel;
 function preload(){
   trex_correndo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   ground2=loadImage("ground2.png");
@@ -11,6 +11,8 @@ function setup(){
   
   //crie um sprite de trex
   trex = createSprite(50,100,20,50);
+  chao_invisivel=createSprite(200,190,400,20);
+  chao_invisivel.visible=false;
   trex.addAnimation("correndo", trex_correndo);
   trex.scale=0.6
   chao=createSprite(200,180,400,20);
@@ -19,14 +21,15 @@ function setup(){
 }
 
 function draw(){
-  trex.collide(chao);
+  trex.collide(chao_invisivel);
+  console.log(trex.y);
   pulo();
   chao_movimentando();
   background("white");
   drawSprites();
 }
 function pulo(){
-  if(keyDown("up")){
+  if(keyDown("up") && trex.y>151){
     trex.velocityY=-10;
   }
   trex.velocityY+=0.8;
